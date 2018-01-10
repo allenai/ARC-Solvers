5a95d468f42b7fe1b69a53bf41854e071080dc69
Tushar Khot <tushark@allenai.org>
2018-01-09 16:23:37 -0800

    Fix overrides

--- asq_solvers/commands/__init__.py
+++ asq_solvers/commands/__init__.py
@@ -3,12 +3,13 @@ from allennlp.commands import main as main_allennlp
 
 
 def main(prog: str = None) -> None:
-    subcommand_overrides = {
-        "predict_custom": PredictCustom()
-    }
     predictor_overrides = {
-        "decomposable_attention": "decompatt"
+        "decomposable_attention": "decompatt",
     }
+    subcommand_overrides = {
+        "predict_custom": PredictCustom(predictor_overrides=predictor_overrides)
+    }
+
     main_allennlp(prog,
                   predictor_overrides=predictor_overrides,
                   subcommand_overrides=subcommand_overrides)
