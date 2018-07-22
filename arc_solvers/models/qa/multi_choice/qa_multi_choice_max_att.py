51c3266d0f01f46e5955d41bf05cee11d04851ee
Todor Mihaylov <tbmihailov@gmail.com>
2018-07-23 01:20:44 +1000

    Refactoring and code formatting

--- arc_solvers/models/qa/multi_choice/qa_multi_choice_max_att.py
+++ arc_solvers/models/qa/multi_choice/qa_multi_choice_max_att.py
@@ -11,8 +11,7 @@ from allennlp.modules import TextFieldEmbedder
 from allennlp.nn import InitializerApplicator
 from allennlp.training.metrics import CategoricalAccuracy
 
-from arc_solvers.common.common_utils import update_params
-from arc_solvers.nn.util import embedd_encode_and_aggregate_list_text_field, embedd_encode_and_aggregate_text_field
+from arc_solvers.nn.util import embed_encode_and_aggregate_list_text_field, embed_encode_and_aggregate_text_field
 
 
 @Model.register("qa_multi_choice_max_att")
@@ -20,7 +19,8 @@ class QAMultiChoiceMaxAttention(Model):
     """
     This ``Model`` implements a simple context encoder baseline that models the interaction between question and choice:
     The basic outline of this model is to get an embedded representation for the
-    question and choice, model an interaction between them and use a linear projection to the class dimension + softmax to get a final predictions:
+    question and choice, model an interaction between them and use a linear projection to the class dimension + softmax
+    to get a final predictions:
 
     question_encoded = context_enc(question_words)  # context encoder can be any AllenNLP supported or None
     choice_encoded = context_enc(choice_words)
@@ -28,9 +28,11 @@ class QAMultiChoiceMaxAttention(Model):
     question_aggregate = aggregate_method(question_encoded)
     choice_aggregate = aggregate_method(choice_encoded)
 
-    inter = concat([question_aggregate, choice_aggregate, abs(choice_aggregate - question_aggregate), question_aggregate * choice_aggregate)
+    inter = concat([question_aggregate, choice_aggregate, abs(choice_aggregate - question_aggregate), question_aggregate
+     * choice_aggregate)
 
-    The model is a popular baseline but it is most simialar to Conneau, A. et al. (2017) ‘Supervised Learning of Universal Sentence Representations from Natural Language Inference Data’,
+    The model is a popular baseline but it is most simialar to Conneau, A. et al. (2017) ‘Supervised Learning of
+    Universal Sentence Representations from Natural Language Inference Data’,
     without the feed-forward layer!
 
     Parameters
@@ -109,11 +111,7 @@ class QAMultiChoiceMaxAttention(Model):
 
         # question to choice attention
         att_question_to_choice_params = params.get("att_question_to_choice")
-        if "tensor_1_dim" in att_question_to_choice_params:
-            # automatically infer the input size
-            att_question_to_choice_params = update_params(att_question_to_choice_params,
-                                                          {"tensor_1_dim": question_output_dim,
-                                                           "tensor_2_dim": choice_output_dim})
+
         self._matrix_attention_question_to_choice = MatrixAttention(
             SimilarityFunction.from_params(att_question_to_choice_params))
 
@@ -153,17 +151,16 @@ class QAMultiChoiceMaxAttention(Model):
             A scalar loss to be optimised.
         """
 
-        encoded_choices_aggregated = embedd_encode_and_aggregate_list_text_field(choices_list,
-                                                                                 self._text_field_embedder,
-                                                                                 self._embeddings_dropout,
-                                                                                 self._choice_encoder,
-                                                                                 self._choice_aggregate)  # # bs, choices, hs
-
-        encoded_question_aggregated, _ = embedd_encode_and_aggregate_text_field(question, self._text_field_embedder,
+        encoded_choices_aggregated = embed_encode_and_aggregate_list_text_field(choices_list,
+                                                                                self._text_field_embedder,
                                                                                 self._embeddings_dropout,
-                                                                                self._question_encoder,
-                                                                                self._question_aggregate,
-                                                                                get_last_states=False)  # bs, hs
+                                                                                self._choice_encoder,
+                                                                                self._choice_aggregate)  # # bs, choices, hs
+
+        encoded_question_aggregated = embed_encode_and_aggregate_text_field(question, self._text_field_embedder,
+                                                                               self._embeddings_dropout,
+                                                                               self._question_encoder,
+                                                                               self._question_aggregate)  # bs, hs
 
         q_to_choices_att = self._matrix_attention_question_to_choice(encoded_question_aggregated.unsqueeze(1),
                                                                      encoded_choices_aggregated).squeeze()
