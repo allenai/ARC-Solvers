73818945dd2035a40cd7df03212a2233919275b8
Todor Mihaylov <tbmihailov@gmail.com>
2018-07-23 01:00:15 +1000

    Reverted to original version with AllenNLP 0.2

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -5,6 +5,4 @@ pip install -r requirements.txt
 # Copied from AllenNLP
 # Temporary fix to the build whilst NLTK sort stuff out.
 python -m nltk.downloader -u https://pastebin.com/raw/D3TBY4Mj punkt
-# python -m spacy.en.download all spacy <2.0
-
-python -m spacy download en
+python -m spacy.en.download all
