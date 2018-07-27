df22d9a56b1118f8172308fb8f8c2ded28637a97
Harish Thuwal <hthuwal@gmail.com>
2018-07-27 13:38:35 +0530

    Add lines to debug code line by line

--- scripts/evaluate_solver.sh
+++ scripts/evaluate_solver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-
+set -x
+trap read debug
 # ----------------------------------------------------------------
 # Evaluate an entailment model (DGEM/DecompAtt) on the QA dataset
 # ----------------------------------------------------------------
